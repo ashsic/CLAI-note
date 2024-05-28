c3459a728806553419220fa2bbeaac4e177f42c9
ashsic <99445200+ashsic@users.noreply.github.com>
2024-05-28 13:53:13 -0400

    renaming and removing old files

---
+++ CLAI-note/database.py
@@ -0,0 +1,23 @@
+"""This module provides basic local flatfile JSON storage."""
+
+import configparser
+from pathlib import Path
+
+from __init__ import DB_WRITE_ERROR, SUCCESS
+
+DEFAULT_DB_FILE_PATH = Path.home().joinpath(
+    "." + Path.home().stem + "_todo.json"
+)
+
+def get_database_path(config_file: Path) -> Path:
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+def init_database(db_path: Path) -> int:
+    # Creates db
+    try:
+        db_path.write_text("[]") # Create empty db
+        return SUCCESS
+    except OSError:
+        return DB_WRITE_ERROR
