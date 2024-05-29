868a5f8683b86886dc529e45e5f6572b41c5e2a6
ashsic <99445200+ashsic@users.noreply.github.com>
2024-05-29 15:41:15 -0400

    adding main function

---
+++ CLAInote/main.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+import typer
+from typing_extensions import Annotated
+
+
+def main(name: Annotated[Optional[str], typer.Argument()]=None):
+    """
+    test docstring, check using --help
+    """
+    if name is None:
+        print("Hello World!")
+    else:
+        print(f"Hello {name}!")
+
+if __name__ == "__main__":
+    typer.run(main)
